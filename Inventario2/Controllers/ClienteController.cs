f6c1fadc6602d1de22e429ebc60ab32c49544208
javierortegon <javierortegonmp@gmail.com>
2020-08-24 19:43:14 -0500

    Agregando crud de usuario

---
+++ Inventario2/Controllers/ClienteController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Inventario2.Models;
+
+namespace Inventario2.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Inventario2/Controllers/UsuarioController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Inventario2.Models;
+
+namespace Inventario2.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            using (var db = new inventarioEntities())
+            {
+                return View(db.usuario.ToList());
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            using (var db = new inventarioEntities())
+            {
+                usuario objUsuario = db.usuario.Where(a => a.id == id).FirstOrDefault();
+                return View(objUsuario);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(usuario usuario)
+        {
+            try
+            {
+                using (var db = new inventarioEntities())
+                {
+                    usuario objUsuario = db.usuario.Find(usuario.id);
+                    objUsuario.nombre = usuario.nombre;
+                    objUsuario.apellido = usuario.apellido;
+                    objUsuario.email = usuario.email;
+                    objUsuario.password = usuario.password;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(usuario usuario)
+        {
+            if (!ModelState.IsValid)
+                return View();
+            try
+            {
+                using (var db = new inventarioEntities())
+                {
+                    db.usuario.Add(usuario);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+                throw;
+            }
+        }
+
+    }
+}
