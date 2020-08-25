924a07ce020378d476c309db8d4683e9fe3a5794
javierortegon <javierortegonmp@gmail.com>
2020-08-24 20:42:43 -0500

    registro y consulta de productos con llave foranea

---
+++ Inventario2/Controllers/ProductoController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Inventario2.Models;
+
+namespace Inventario2.Controllers
+{
+    public class ProductoController : Controller
+    {
+        // GET: Producto
+        public ActionResult Index()
+        {
+            using (var db = new inventarioEntities())
+            {
+                return View(db.producto.ToList());
+            }
+        }
+        
+        public ActionResult listarProveedores()
+        {
+            using (var db = new inventarioEntities())
+            {
+                return PartialView(db.proveedor.ToList());
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(producto producto)
+        {
+            if (!ModelState.IsValid)
+                return View();
+            try
+            {
+                using (var db = new inventarioEntities())
+                {
+                    db.producto.Add(producto);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+                throw;
+            }
+        }
+
+        public static string NombreProveedor(int? idProveedor)
+        {
+            using (var db = new inventarioEntities())
+            {
+                return db.proveedor.Find(idProveedor).nombre;
+            }
+        }
+
+    }
+}
