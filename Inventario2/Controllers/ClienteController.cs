c6cd93f463d796620b0f3ca2c9ea85da72b58bdb
javierortegon <javierortegonmp@gmail.com>
2020-08-24 20:04:31 -0500

    terminando crud basicos

--- Inventario2/Controllers/ClienteController.cs
+++ Inventario2/Controllers/ClienteController.cs
@@ -12,7 +12,70 @@ namespace Inventario2.Controllers
         // GET: Cliente
         public ActionResult Index()
         {
+            using (var db = new inventarioEntities())
+            {
+                return View(db.cliente.ToList());
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            using (var db = new inventarioEntities())
+            {
+                cliente objCliente = db.cliente.Where(a => a.id == id).FirstOrDefault();
+                return View(objCliente);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(cliente cliente)
+        {
+            try
+            {
+                using (var db = new inventarioEntities())
+                {
+                    cliente objCliente = db.cliente.Find(cliente.id);
+                    objCliente.nombre = cliente.nombre;
+                    objCliente.documento = cliente.documento;
+                    objCliente.email = cliente.email;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+        public ActionResult Create()
+        {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(cliente cliente)
+        {
+            if (!ModelState.IsValid)
+                return View();
+            try
+            {
+                using (var db = new inventarioEntities())
+                {
+                    db.cliente.Add(cliente);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+                throw;
+            }
+        }
+
     }
 }
